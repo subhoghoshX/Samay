fb1fede422eb3b7d9dc11773fd5883e59d6bffdf
Subho <contact.subhoghosh@gmail.com>
2024-05-30 17:33:05 +0530

    move to typescript

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./popup/**/*.{html,js,jsx}"],
+  content: ["./popup/**/*.{html,ts,tsx}"],
   theme: {
     extend: {},
   },
