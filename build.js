5f0c3fa78d270f51941e3b3aa4c9e879c65220e3
Subho <contact.subhoghosh@gmail.com>
2024-05-30 16:52:53 +0530

    add prettier

--- build.js
+++ build.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 
 // based on build.js from https://github.com/cockpit-project
-import fs from 'node:fs';
-import path from 'node:path';
-import esbuild from 'esbuild';
-import copy from 'esbuild-plugin-copy';
-import postcss from 'esbuild-postcss';
-import { ArgumentParser } from 'argparse';
+import fs from "node:fs";
+import path from "node:path";
+import esbuild from "esbuild";
+import copy from "esbuild-plugin-copy";
+import postcss from "esbuild-postcss";
+import { ArgumentParser } from "argparse";
 
 const development = process.env.NODE_ENV === "development";
 
 const context = await esbuild.context({
-  ...development ? { sourcemap: "linked" }: {},
+  ...(development ? { sourcemap: "linked" } : {}),
   bundle: true,
   entryPoints: ["./popup/index.js", "./popup/tailwind.css"],
   outdir: "dist/popup",
@@ -23,14 +23,14 @@ const context = await esbuild.context({
       setup(build) {
         build.onStart(() => {
           try {
-            fs.rmSync(path.resolve("./dist"), { recursive: true })
-          } catch(error) {
-            if(error.code !== "ENOENT") {
+            fs.rmSync(path.resolve("./dist"), { recursive: true });
+          } catch (error) {
+            if (error.code !== "ENOENT") {
               throw error;
             }
           }
-        })
-      }
+        });
+      },
     },
     postcss(),
     copy({
@@ -38,60 +38,63 @@ const context = await esbuild.context({
         // `to` paths are relative to `outdir`, `from` paths are relative to `build.js`.
         { from: ["./popup/index.html"], to: ["index.html"] },
         { from: ["./manifest.json"], to: ["../manifest.json"] },
-      ]
-    })
-  ]
+      ],
+    }),
+  ],
 });
 
 try {
   await context.rebuild();
-} catch(e) {
+} catch (e) {
   console.log(e);
 }
 
 const parser = ArgumentParser();
-parser.add_argument("-w", "--watch", { action: "store_true", help: "Enable watch mode" })
+parser.add_argument("-w", "--watch", {
+  action: "store_true",
+  help: "Enable watch mode",
+});
 const args = parser.parse_args();
 
 // similar to fs.watch(), but recursively watches all subdirectories
 function watch_dirs(dir, on_change) {
-    const callback = (ev, dir, fname) => {
-        // only listen for "change" events, as renames are noisy
-        // ignore hidden files and the "4913" temporary file create by vim
-        const isHidden = /^\./.test(fname);
-        if (ev !== "change" || isHidden || fname === "4913") {
-            return;
-        }
-        on_change(path.join(dir, fname));
-    };
-
-    fs.watch(dir, {}, (ev, path) => callback(ev, dir, path));
-
-    // watch all subdirectories in dir
-    const d = fs.opendirSync(dir);
-    let dirent;
-
-    while ((dirent = d.readSync()) !== null) {
-        if (dirent.isDirectory())
-            watch_dirs(path.join(dir, dirent.name), on_change);
+  const callback = (ev, dir, fname) => {
+    // only listen for "change" events, as renames are noisy
+    // ignore hidden files and the "4913" temporary file create by vim
+    const isHidden = /^\./.test(fname);
+    if (ev !== "change" || isHidden || fname === "4913") {
+      return;
     }
-    d.closeSync();
+    on_change(path.join(dir, fname));
+  };
+
+  fs.watch(dir, {}, (ev, path) => callback(ev, dir, path));
+
+  // watch all subdirectories in dir
+  const d = fs.opendirSync(dir);
+  let dirent;
+
+  while ((dirent = d.readSync()) !== null) {
+    if (dirent.isDirectory())
+      watch_dirs(path.join(dir, dirent.name), on_change);
+  }
+  d.closeSync();
 }
 
 if (args.watch) {
-    const on_change = async path => {
-        console.log("change detected:", path);
-        await context.cancel();
+  const on_change = async (path) => {
+    console.log("change detected:", path);
+    await context.cancel();
 
-        try {
-            await context.rebuild();
-        } catch (e) {} // ignore in watch mode
-    };
+    try {
+      await context.rebuild();
+    } catch (e) {} // ignore in watch mode
+  };
 
-    watch_dirs('popup', on_change);
+  watch_dirs("popup", on_change);
 
-    // wait forever until Control-C
-    await new Promise(() => {});
+  // wait forever until Control-C
+  await new Promise(() => {});
 }
 
 context.dispose();
