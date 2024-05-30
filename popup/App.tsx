be1d5d17dce14ca0f8d8d673424753c4da7ba61a
Subho <contact.subhoghosh@gmail.com>
2024-05-30 19:33:16 +0530

    add shadcn
    fix two ts errors along the way

--- popup/App.tsx
+++ popup/App.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import * as React from "react";
+import { Terminal } from "lucide-react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 export default function App() {
   return (
-    <main className="w-80 px-20 py-10">
-      <h1 className="font-bold text-lg">Hello from Samay!!</h1>
+    <main className="w-80 px-5 py-4">
+      <Alert>
+        <Terminal className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>Hello from shadcn!</AlertDescription>
+      </Alert>
     </main>
   );
 }
