a98ed1bdb0d7d0500be632f3263301f340b6c337
Subho <contact.subhoghosh@gmail.com>
2024-07-26 10:39:22 +0530

    Add playwright test
    - the playwright.config.ts is generated by `npm init playwright@latest`
    - the test job is inspired by generated .github/workflows/playwright.yml
      file

---
+++ tests/example.spec.ts
@@ -0,0 +1,43 @@
+import { test as base, type BrowserContext } from "@playwright/test";
+import { chromium } from "playwright";
+import path from "node:path";
+
+// @ts-ignore: stop typescript from bitching about import.meta
+const __dirname = path.dirname(import.meta.filename);
+
+const test = base.extend<{ context: BrowserContext; extensionId: string }>({
+  // biome-ignore lint: not use what this object destructuring is for
+  context: async ({}, use) => {
+    const pathToExtension = path.join(__dirname, "../dist");
+    const context = await chromium.launchPersistentContext("", {
+      headless: false,
+      args: [
+        "--headless=new",
+        `--disable-extensions-except=${pathToExtension}`,
+        `--load-extension=${pathToExtension}`,
+      ],
+    });
+    await use(context);
+    await context.close();
+  },
+  extensionId: async ({ context }, use) => {
+    let [background] = context.serviceWorkers();
+    if (!background) {
+      background = await context.waitForEvent("backgroundpage");
+    }
+    const exntesionId = background.url().split("/")[2];
+    await use(exntesionId);
+  },
+});
+
+const expect = test.expect;
+
+test("Extension in New Tab page", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+  await page.waitForTimeout(3000);
+  await page.goto("chrome://newtab");
+  await expect(page).toHaveTitle("New Tab");
+  await expect(page).toHaveScreenshot({
+    mask: [page.locator("section > div:last-child > div:first-child svg")],
+  });
+});
